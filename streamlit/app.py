e136c00058351bae9fead21235d5b49cd0a10a93
amahdin <81641182+amahdin@users.noreply.github.com>
2024-07-19 00:07:58 -0700

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -19,7 +19,7 @@ st.write(f"Current working directory: {os.getcwd()}")
 # Load model, set cache to prevent reloading
 @st.cache_resource(show_spinner=True)
 def load_model():
-    model = keras.models.load_model("/home/ubuntu/datathon/models/dinosaur_classifier.keras")
+    model = keras.models.load_model("/models/dinosaur_classifier.keras")
     return model
 
 def load_image(image):
@@ -46,7 +46,7 @@ def return_color_generator(NUM_COLORS, cmap='terrain'):
 # SOURCE: BugBytes from YouTube
 # caching data to avoid reloading data every time the page is refreshed with decorator
 @st.cache_data
-def read_data(data_loc='/home/ubuntu/datathon/data/filtered_dino_fossil_locations.csv'):
+def read_data(data_loc='data/filtered_dino_fossil_locations.csv'):
     data = list()
 
     # data frame of dinosaur fossil locations across the world
@@ -68,19 +68,6 @@ def read_data(data_loc='/home/ubuntu/datathon/data/filtered_dino_fossil_location
 
     return dino_fossil_locations, dino_class_to_color
 
-def add_legend(map_obj, class_to_color):
-    legend_html = '''
-    <div style="position: fixed;
-                bottom: 50px; left: 50px; width: 200px; height: auto;
-                background-color: white; z-index:9999; font-size:14px;
-                border:2px solid grey; border-radius:8px; padding: 10px;">
-                <h4 style="margin-top:0;">Legend</h4>
-    '''
-    for dino_class, color in class_to_color.items():
-        legend_html += f'<i style="background:{color}; width: 20px; height: 20px; display: inline-block;"></i> {dino_class}<br>'
-
-    legend_html += '</div>'
-    map_obj.get_root().html.add_child(folium.Element(legend_html))
 
 def main():
     # Create page header and description
@@ -113,7 +100,7 @@ def main():
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     # Dropdown for preselected images
-    test_imgs_dir = '/home/ubuntu/datathon/test_imgs'
+    test_imgs_dir = 'test_imgs'
     if os.path.exists(test_imgs_dir):
         preselected_images = os.listdir(test_imgs_dir)
         selected_image = st.selectbox("Or select a preselected image", preselected_images)
